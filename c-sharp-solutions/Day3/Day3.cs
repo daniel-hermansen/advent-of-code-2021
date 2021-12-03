f5e037a96044bf03aa2d73069dd30633be57a53f
Alex Mertlich <alex.mertlich@junipersys.com>
2021-12-03 11:43:15 -0700

    Simplifying the Day3 part1 solution and making it faster.

--- c-sharp-solutions/Day3/Day3.cs
+++ c-sharp-solutions/Day3/Day3.cs
@@ -1,49 +1,33 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace Merry.Christmas
 {
     public class Day3 : PuzzleSolver
     {
         protected override string Solve(string[] inputLines)
         {
-            var bitmap = new Dictionary<int, (int falseCount, int trueCount)>();
-            foreach (var line in inputLines)
-            {
-                for (int i = 0; i < line.Length; i++)
-                {
-                    char ch = line[i];
-                    var currentValues = ((ch == '0' ? 1 : 0),ch == '1' ? 1 : 0);
-                    if (bitmap.ContainsKey(i))
-                    {
-                        bitmap[i] = (bitmap[i].falseCount + currentValues.Item1, bitmap[i].trueCount + currentValues.Item2);
-                    }
-                    else
-                    {
-                        bitmap[i] = currentValues;
-                    }
-                    
-                }
-            }
             int gamma = 0;
             int epsilon = 0;
-            foreach (var bit in bitmap)
+            for (int i = 0; i < inputLines[0].Length; i++)
             {
-                gamma = gamma << 1;
-                epsilon = epsilon << 1;
-                if (bit.Value.trueCount > bit.Value.falseCount)
+                int totalSetBits = 0;
+                foreach (var line in inputLines)
+                {
+                    if (line[i] != '0') totalSetBits++;
+                }
+
+                gamma <<= 1;
+                epsilon <<= 1;
+
+                if (totalSetBits >= inputLines.Length / 2)
                 {
                     gamma += 1;
-                    Console.Write("1");
                 }
                 else
                 {
                     epsilon += 1;
-                    Console.Write("0");
                 }
             }
-            return $"{gamma*epsilon}";
+
+            return $"{gamma * epsilon}";
         }
     }
 }
